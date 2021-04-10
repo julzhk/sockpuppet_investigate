ded484e0ca531bb718d8b1b372e475aab3e62fb8
julian <julian.harley@gmail.com>
2021-04-10 13:45:21 +0100

    logical swap included fragments

--- picker/views/picker_reflex.py
+++ picker/views/picker_reflex.py
@@ -9,7 +9,9 @@ class PickerReflexView(TemplateView):
         context = super().get_context_data(*args, **kwargs)
         context['count'] = self.request.session.get('count', 3)
         context['loop'] = range(0, 200 * context['count'])
+        context['inc_template'] = 'included.html'
 
-        if context.get('count',0) > 4:
+        if context.get('count', 0) > 4:
+            context['inc_template'] = 'included2.html'
             context['msg'] = 'overload'
         return context
